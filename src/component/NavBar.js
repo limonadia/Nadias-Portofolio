f2bc8f0ee6c6ae1a0da708f44f6df7938015a03c
Nadia Mezini <nadia@MacBook-Air.local>
2024-03-07 14:11:25 -0800

    initial commit

---
+++ src/component/NavBar.js
@@ -0,0 +1,46 @@
+import {useState, useEffect} from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/n-letter.jpg';
+import React from 'react';
+
+const CustomNavBar=() =>{
+    const [activeLink, setActiveLink]=useState('home');
+    const [scrolled, seScrolled]=useState(false);
+    useEffect(()=>{
+        const onScroll = ()=> {
+            if (window.scrollY >50){
+                seScrolled(true);
+            }else{
+                seScrolled(false);
+            }
+        }
+        window.addEventListener("scroll", onScroll);
+        return()=> window.removeEventListener("scroll", onScroll);
+    }, [])
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+            <img src={logo} alt="Logo"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('skills')}>Skills</Nav.Link>
+          </Nav>
+          <button className="vvd" onClick={()=> console.log('connect')}><span>Let's Connect</span></button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default CustomNavBar;
